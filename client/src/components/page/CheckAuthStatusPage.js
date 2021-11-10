b1d1e56962500e8654a9c1e918a9d31871739dea
JacksonTrudel <jtrudel97@gmail.com>
2021-11-10 04:46:57 +0000

    Minimized CheckAuthenticationStatus action

--- client/src/components/page/CheckAuthStatusPage.js
+++ client/src/components/page/CheckAuthStatusPage.js
@@ -8,13 +8,17 @@ function CheckAuthStatusPage() {
     const login = useSelector((state) => state.user);
 
     // call only when component mounts
-    useEffect(() => checkAuthenticationStatus(setUser), [setUser]);
+    useEffect(() => checkAuthenticationStatus((response) => {
+        // Account successfully created
+        if (response.status === 200 && response.data.user.loggedIn !== false) {
+            setUser(response.data.user);
+        }
+    }), [setUser]);
 
     return (<>
         <div>Client-side login state: {JSON.stringify(login)}</div>
         <div>Server-side login state: {JSON.stringify(user)}</div>
     </>);
 }
-
 
 export default CheckAuthStatusPage;
--- client/src/user_actions/CheckAuthenticationStatus.js
+++ client/src/user_actions/CheckAuthenticationStatus.js
@@ -1,13 +1,10 @@
 import axios from 'axios';
 
-export default function checkAuthenticationStatus(setUser) {
+export default function checkAuthenticationStatus(callback) {
     // try to access protected route     
     axios.post('http://localhost:8082/accounts/user', null, { withCredentials: true })
         .then(res => {
-            // Account successfully created
-            if (res.status === 200 && res.data.user.loggedIn !== false) {
-                setUser(res.data.user);
-            }
+            callback(res);
         }).catch((error) => {
             alert("Unexpected error. Could not check authentication.");
             console.log(error);
